67e950856391f8fa49f698942b29930c2739409a
Guilherme <guilhermego1997@gmail.com>
2019-11-16 11:44:49 -0300

    delete folders in project

--- FrontEnd/src/app/pages/categories/categories.module.ts
+++ FrontEnd/src/app/pages/categories/categories.module.ts
@@ -6,6 +6,8 @@ import { CategoriesRoutingModule } from './categories-routing.module';
 import { CategoryListComponent } from './category-list/category-list.component';
 import { CategoryFormComponent } from './category-form/category-form.component';
 
+
+
 @NgModule({
   declarations: [ CategoryListComponent, CategoryFormComponent],
   imports: [
