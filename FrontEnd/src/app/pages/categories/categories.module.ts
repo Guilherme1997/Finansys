886a3ff226adef901ad8067b1212dfe9ffe843b7
Guilherme <guilhermego1997@gmail.com>
2019-11-16 14:36:03 -0300

    adding logic edit and create form

--- FrontEnd/src/app/pages/categories/categories.module.ts
+++ FrontEnd/src/app/pages/categories/categories.module.ts
@@ -2,17 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CategoriesRoutingModule } from './categories-routing.module';
-//import { CategoryComponent } from './category/category.component';
 import { CategoryListComponent } from './category-list/category-list.component';
 import { CategoryFormComponent } from './category-form/category-form.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [ CategoryListComponent, CategoryFormComponent],
   imports: [
     CommonModule,
-    CategoriesRoutingModule
+    CategoriesRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class CategoriesModule { }
